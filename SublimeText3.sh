22f8d1f425491e6343c0cefd4befa6a734d5aa53
k0ttee <k0ttee@yandex.ru>
2021-05-15 10:12:29 +0300

    Create SublimeText3.sh

---
+++ SublimeText3.sh
@@ -0,0 +1,34 @@
+#запрет обновлений
+
+C:\Windows\System32\drivers\etc\hosts
+/etc/hosts
+
+127.0.0.1 www.sublimetext.com
+127.0.0.1 sublimetext.com
+127.0.0.1 sublimehq.com
+127.0.0.1 license.sublimehq.com
+127.0.0.1 45.55.255.55
+127.0.0.1 45.55.41.223
+0.0.0.0 license.sublimehq.com
+0.0.0.0 45.55.255.55
+0.0.0.0 45.55.41.223
+
+#конфиг
+
+preferences → settings
+⌘,
+
+{
+	"font_size": 14,
+	"ignored_packages":
+	[
+		"Vintage"
+	],
+	"update_check": false
+}
+
+#пакетный менеджер
+
+Package Control
+INI
+NGINX
